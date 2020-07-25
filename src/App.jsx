ff69cfe3636133d4601884aa3f58bef6850f999b
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-07-26 00:55:07 +0100

    Now pulling year 3 results from json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React, {Component} from 'react';
 import './App.scss';
 import logo from './logo.png';
+import resultsYear3 from './jsons/resultsYear3.json';
 import BarChart from './components/BarChart';
 
 let d3js = 'https://d3js.org/';
-let resultsYear3 = [
-    {name: "Computer Security and Cryptography", value: 92},
-    {name: "Computer Networks and Communication", value: 88},
-    {name: "Programming Language Implementation", value: 88},
-    {name: "Programming Languages: Application and Design", value: 88},
-    {name: "Data Mining and Knowledge Discovery", value: 82},
-    {name: "Research Project", value: 72},
-    {name: "Internet of Things", value: 70}
-]
 
 class App extends Component {
     render() {
@@ -30,7 +22,7 @@ class App extends Component {
                     <p>A data-driven visualisation of the Education section of my CV.</p>
                 </div>
                 <div className="uni_year_3">
-                    <p>A data-driven visualisation of my University of Kent (year 3) results from Uni, as shown on my CV.</p>
+                    <p>A data-driven visualisation of my final year results from University of Kent, as shown on my CV.</p>
                     <BarChart data={resultsYear3}/>
                 </div>
             </div>
